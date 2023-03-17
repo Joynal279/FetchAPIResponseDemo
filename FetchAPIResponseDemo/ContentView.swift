7d9af76a653ab4b4424c5f5ac037b5a8ef885a00
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-17 16:01:49 +0600

    Initial Commit

---
+++ FetchAPIResponseDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FetchAPIResponseDemo
+//
+//  Created by Joynal Abedin on 17/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FetchAPIResponseDemo/FetchAPIResponseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAPIResponseDemoApp.swift
+//  FetchAPIResponseDemo
+//
+//  Created by Joynal Abedin on 17/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchAPIResponseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
