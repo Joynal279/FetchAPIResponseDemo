0b17eb35c1f46ed4d96fa7ffe056c774c3257852
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-17 19:53:15 +0600

    initial commit

--- FetchAPIResponseDemo/ContentView.swift
+++ FetchAPIResponseDemo/ContentView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var results = [EntryTask]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        List(results, id: \.id) { data in
+            VStack(alignment: .leading) {
+                Text(data.title)
+            }
+        }.onAppear(perform: loadData)
+        
     }
+    
+    func loadData(){
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else {
+            print("Invalid API End Point")
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let response = try? JSONDecoder().decode([EntryTask].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = response
+                    }
+                    return
+                }
+            }
+        }.resume()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ FetchAPIResponseDemo/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  FetchAPIResponseDemo
+//
+//  Created by Joynal Abedin on 17/3/23.
+//
+
+import SwiftUI
+
+struct EntryTask: Codable {
+    var id: Int
+    var title: String
+}
